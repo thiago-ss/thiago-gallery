36c70c111931629151c64e20afea8e8e7e4c68b8
Thiago Schweder Souza <thiago2508ss@gmail.com>
2024-07-12 20:06:49 -0300

    Scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+import Image from "next/image";
+
+const mockData = [
+  "https://utfs.io/f/62ea6c44-9114-4cb1-8ad8-7cb089367ca8-qr2g2l.59.25.jpeg",
+  "https://utfs.io/f/38ad3a21-0117-4263-8a69-b5b65a575ab7-qplv5p.35.34.jpeg",
+  "https://utfs.io/f/dfaadbe6-78a4-4247-9e01-86dd0513bbe3-1jb3ov.jpg",
+];
+
+const mockImages = mockData.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Gallery in progress</h1>
+    <main>
+      <div className="flex flex-col gap-10 justify-center items-center">
+        <h1 className="text-3xl font-bold text-center">Image Gallery</h1>
+        <div className="flex flex-wrap gap-4 justify-center">
+          {mockImages.map((image) => (
+            <Image
+              width={300}
+              height={300}
+              key={image.id}
+              src={image.url}
+              alt={`Image ${image.id}`}
+              className=" object-cover rounded-md"
+            />
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
